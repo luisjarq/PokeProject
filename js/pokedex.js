92dd1776e4ab93bb0fca329d9d0cb6ee67f29487
Luisjarq <lc.luisja@gmail.com>
2021-11-04 19:32:14 +0100

    Refactor

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,34 +1,33 @@
 window.onload = run;
 async function loadPokemonAsync(limit = 151) {
   console.log("fetching pokemons");
-  let response = await fetch(
+  const response = await fetch(
     `https://pokeapi.co/api/v2/pokemon/?limit=${limit}`
   );
-  let json = await response.json();
+  const json = await response.json();
   return json.results;
 }
 async function loadPokemonData(pokemon) {
-  let response = await fetch(pokemon.url);
-  let json = await response.json();
+  const response = await fetch(pokemon.url);
+  const json = await response.json();
   return json;
 }
 async function run() {
-  let pokemons = await loadPokemonAsync(151);
+  const pokemons = await loadPokemonAsync(151);
   for (const pokemon of pokemons) {
-    let pokeData = await loadPokemonData(pokemon);
-    let types = pokeData.types.map((t) => t.type.name.capitalize()).join(" ");
-    let className = `div_${pokeData.types.map((t) => t.type.name).join("")}`;
-    let imageNew = pokeData.sprites.other.dream_world.front_default;
-    let imageClassic = pokeData.sprites.front_default;
-    let content = document.querySelector(".content");
-    let pokeDiv = document.createElement("div");
+    const pokeData = await loadPokemonData(pokemon);
+    const types = pokeData.types.map((t) => t.type.name.capitalize()).join(" ");
+    const className = `div_${pokeData.types.map((t) => t.type.name).join("")}`;
+    const imageNew = pokeData.sprites.other.dream_world.front_default;
+    const content = document.querySelector(".content");
+    const pokeDiv = document.createElement("div");
     pokeDiv.classList.add(className);
     pokeDiv.classList.add('target');
-    let img = document.createElement("img");
+    const img = document.createElement("img");
     img.src = imageNew;
     img.alt = `${pokemon.name}_${pokeData.id}`;
     img.classList.add("zoom");
-    let info = document.createElement("div");
+    const info = document.createElement("div");
     info.innerHTML = `
     <p class="info"># ${pokeData.id}</p>
     <p class="info">Name: ${pokemon.name.capitalize()}</p>
