f3680c46d57dc0b62711e73325fe91c6619c2f94
Luisjarq <lc.luisja@gmail.com>
2021-11-06 11:36:31 +0100

    Refactor#2

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,4 +1,4 @@
-window.onload = run;
+window.onload = loadAll;
 async function loadPokemonAsync(limit = 151) {
   console.log("fetching pokemons");
   const response = await fetch(
@@ -7,37 +7,37 @@ async function loadPokemonAsync(limit = 151) {
   const json = await response.json();
   return json.results;
 }
-async function loadPokemonData(pokemon) {
+async function loadPokemonDataAsync(pokemon) {
   const response = await fetch(pokemon.url);
   const json = await response.json();
   return json;
 }
-async function run() {
-  const pokemons = await loadPokemonAsync(151);
-  for (const pokemon of pokemons) {
-    const pokeData = await loadPokemonData(pokemon);
-    const types = pokeData.types.map((t) => t.type.name.capitalize()).join(" ");
-    const className = `div_${pokeData.types.map((t) => t.type.name).join("")}`;
-    const imageNew = pokeData.sprites.other.dream_world.front_default;
-    const content = document.querySelector(".content");
-    const pokeDiv = document.createElement("div");
-    pokeDiv.classList.add(className);
-    pokeDiv.classList.add('target');
-    const img = document.createElement("img");
-    img.src = imageNew;
-    img.alt = `${pokemon.name}_${pokeData.id}`;
-    img.classList.add("zoom");
-    const info = document.createElement("div");
-    info.innerHTML = `
+async function createViewAsync(pokemon) {
+  const pokeData = await loadPokemonDataAsync(pokemon);
+  const types = pokeData.types.map((t) => t.type.name.capitalize()).join(" ");
+  const className = `div_${pokeData.types.map((t) => t.type.name).join("")}`;
+  const imageNew = pokeData.sprites.other.dream_world.front_default;
+  const content = document.querySelector(".content");
+  const pokeDiv = document.createElement("div");
+  pokeDiv.classList.add(className);
+  pokeDiv.classList.add("target");
+  const img = document.createElement("img");
+  img.src = imageNew;
+  img.alt = `${pokemon.name}_${pokeData.id}`;
+  img.classList.add("zoom");
+  const info = document.createElement("div");
+  info.innerHTML = `
     <p class="info"># ${pokeData.id}</p>
     <p class="info">Name: ${pokemon.name.capitalize()}</p>
     <p class="info">Type: ${types}</p>
-    <p class="info">Height: ${pokeData.height*10} Cm</p>
-    <p class="info">Weight: ${pokeData.weight/10} Kg</p>
+    <p class="info">Height: ${pokeData.height * 10} Cm</p>
+    <p class="info">Weight: ${pokeData.weight / 10} Kg</p>
     `;
-    console.log(pokeData);
-    pokeDiv.appendChild(img);
-    pokeDiv.appendChild(info);
-    content.appendChild(pokeDiv);
-  }
+  pokeDiv.appendChild(img);
+  pokeDiv.appendChild(info);
+  content.appendChild(pokeDiv);
+}
+async function loadAll() {
+  const pokemons = await loadPokemonAsync(151);
+  pokemons.forEach(createViewAsync);
 }
